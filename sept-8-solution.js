a58be8857a7a9a887c133e463603491375a5e238
nktruong <88857875+nktruong@users.noreply.github.com>
2021-09-09 00:33:40 -0400

    Compacted script
    Converted it into a while loop

--- sept-8-solution.js
+++ sept-8-solution.js
@@ -13,65 +13,23 @@ function addBinary(a,b) {
   
   // if # is smaller than a power of 2, then go
   // if it can go into a #, then subtract the sum
-  while (sum != 0){
-    
-    if (sum >= 128){
-      binary += "1"
-      sum -= 128
-    }
-    // console.log(`Sum: ${sum} Binary: ${binary} at 128`)
-    if (sum >= 64){
-      binary += "1"
-      sum -= 64
-    } else if (binary == "1"){
-      binary += "0"
-    }
-    // console.log(`Sum: ${sum} Binary: ${binary} at 64`)
-    if (sum >= 32){
-      binary += "1"
-      sum -= 32
-    } else {
-      binary += "0"
-    }
-    // console.log(`Sum: ${sum} Binary: ${binary} at 32`)
-    if (sum >= 16){
-      binary += "1"
-      sum -= 16
-    } else {
-      binary += "0"
-    }
-    // console.log(`Sum: ${sum} Binary: ${binary} at 16`)
-    if (sum >= 8){
-      binary += "1"
-      sum -= 8
-    } else {
-      binary += "0"
-    }
-    // console.log(`Sum: ${sum} Binary: ${binary} at 8`)
-    if (sum >= 4){
-      binary += "1"
-      sum -= 4
-    } else {
-      binary += "0"
-    }
-    // console.log(`Sum: ${sum} Binary: ${binary} at 4`)
-    if (sum >= 2){
-      binary += "1"
-      sum -= 2
-    } else {
-      binary += "0"
-    }
-    // console.log(`Sum: ${sum} Binary: ${binary} at 2`)
-    if (sum >= 1){
-      binary += "1"
-      sum -= 1
-    } else {
-      binary += "0"
-    }
-    // console.log(`Sum: ${sum} Binary: ${binary} at 1`)
-  }
-    binary = binary.substring(binary.indexOf("1"));
   
-  console.log(binary)
+  let exp = 7
+  
+  while (sum > 0 || exp > -1){
+    
+    if (sum >= Math.pow(2, exp)){
+      binary += "1"
+      sum -= Math.pow(2, exp)
+    } else {
+      binary += "0"
+    }
+    console.log(`Sum: ${sum} Binary: ${binary} at ${Math.pow(2, exp)}`)
+    exp--
+    if (sum > 0 && exp === -1){
+      exp = 7
+    }
+  }
+  binary = binary.substring(binary.indexOf("1"));
   return binary
 }

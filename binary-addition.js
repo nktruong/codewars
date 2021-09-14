240ae836e72868d319fb91b7a2c444dfe380402b
Nathan Truong <88857875+nktruong@users.noreply.github.com>
2021-09-14 19:00:36 -0400

    Renaming to a more descriptive title

--- binary-addition.js
+++ binary-addition.js
@@ -1,4 +1,5 @@
 // ### Binary Addition
+// Sept 8 2021
 
 // Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
 // The binary number returned should be a string.

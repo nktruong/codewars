483a88f5efc68ca01a82ceaf7b0a250bea10faf0
Nathan Truong <88857875+nktruong@users.noreply.github.com>
2021-09-14 19:04:43 -0400

    Tuesday Codewars completed

---
+++ checkIfSquare-7kyu.js
@@ -0,0 +1,16 @@
+// ### You're a square!
+// Sept 14 2021
+
+// Task: Given an integral number, determine if it's a square number.
+// Examples:
+// -1  =>  false
+//  0  =>  true
+//  3  =>  false
+//  4  =>  true
+// 25  =>  true
+// 26  =>  false
+
+// runs n through Square Root method, checks if there is a remainder to see if it is a perfect square.
+function isSquare(n) {
+  return Math.sqrt(n) & 1 === 0; 
+}

cd215fb9349103b810c93d2c60a9bd0601f92659
Nathan Truong <88857875+nktruong@users.noreply.github.com>
2021-09-14 18:59:27 -0400

    added date

--- sum-of-numbers-n-between.js
+++ sum-of-numbers-n-between.js
@@ -1,4 +1,5 @@
 // ### Beginner Series #3 Sum of Numbers
+// Sept 10 2021
 
 // Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
 // Note: a and b are not ordered!

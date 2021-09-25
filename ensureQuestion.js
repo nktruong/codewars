29c4eb33fcd9d72a19252d079ab30624bdb2b37d
Nathan Truong <88857875+nktruong@users.noreply.github.com>
2021-09-24 20:30:31 -0400

    Conquered ensure question 9/24/2021
    easy, used arrays and bracket notation.
    could make it simpler with ternary operators

---
+++ ensureQuestion.js
@@ -0,0 +1,15 @@
+// Ensure question
+// Given a string, write a function that returns the string with a question mark ("?") appends to the end, unless the original string ends with a question mark, in which case, returns the original string.
+// For example (Input --> Output)
+
+// "Yes" --> "Yes?" 
+// "No?" --> "No?"
+
+function ensureQuestion(s) {
+  // Code here
+  if (s[s.length - 1] !== '?'){
+    return s + '?'
+  } else {
+    return s
+  }
+}

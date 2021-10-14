4a079afd4ff63d8d75341ebcf68ddb167696d31b
Nathan Truong <88857875+nktruong@users.noreply.github.com>
2021-10-13 20:17:55 -0400

    completed Reversed Strings within 60 secs 10/13/2021

---
+++ reversedStrings-8kyu.js
@@ -0,0 +1,7 @@
+// Reversed Strings
+
+// Complete the solution so that it reverses the string passed into it.
+// 'world'  =>  'dlrow'
+function solution(str){
+  return [...str].reverse().join('')
+}

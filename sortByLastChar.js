e20a115f215a0bc98bde0bd8f275d48be143d584
Nathan Truong <88857875+nktruong@users.noreply.github.com>
2021-10-26 20:36:20 -0400

    lexagraphing with characters 10/26/2021

---
+++ sortByLastChar.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/57eba158e8ca2c8aba0002a0
+
+// Sort by Last Char
+
+// Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+// If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+// All inputs will be valid.
+
+function last(x){
+  // split into letters and alphabetize last letters with comparison
+  return x.split(' ').sort((a,b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1))
+}

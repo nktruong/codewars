7e970d1af7346ace9d9410e0e3e6181fae1ad8d3
Nathan Truong <88857875+nktruong@users.noreply.github.com>
2021-11-12 23:17:44 -0500

    Update simpleMultiplication.js

--- simpleMultiplication.js
+++ simpleMultiplication.js
@@ -1,4 +1,6 @@
 // Simple multiplication
+// https://www.codewars.com/kata/583710ccaa6717322c000105
+
 // This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 
 function simpleMultiplication(number) {

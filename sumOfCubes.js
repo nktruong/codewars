90020044a916364cc91773cff730acd2661c13ea
Nathan Truong <88857875+nktruong@users.noreply.github.com>
2021-11-12 23:20:15 -0500

    Update sumOfCubes.js

--- sumOfCubes.js
+++ sumOfCubes.js
@@ -1,6 +1,7 @@
 // Sum of Cubes
-// Write a function that takes a positive integer n, sums all the cubed values from 1 to n, and returns that sum.
+// https://www.codewars.com/kata/59a8570b570190d313000037
 
+// Write a function that takes a positive integer n, sums all the cubed values from 1 to n, and returns that sum.
 // Assume that the input n will always be a positive integer.
 
 // Examples: (Input --> output)

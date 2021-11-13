a4d257e27ba72767c8a0482230e1d882a86f5a28
Nathan Truong <88857875+nktruong@users.noreply.github.com>
2021-11-12 23:18:07 -0500

    Update sortByLastChar.js

--- sortByLastChar.js
+++ sortByLastChar.js
@@ -1,6 +1,5 @@
-// https://www.codewars.com/kata/57eba158e8ca2c8aba0002a0
-
 // Sort by Last Char
+// https://www.codewars.com/kata/57eba158e8ca2c8aba0002a0
 
 // Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
 // If two words have the same last letter, they returned array should show them in the order they appeared in the given string.

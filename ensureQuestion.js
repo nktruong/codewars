1800494d80064fc989c7af979dbe6160a4fa4039
Nathan Truong <88857875+nktruong@users.noreply.github.com>
2021-11-12 22:58:58 -0500

    Update ensureQuestion.js

--- ensureQuestion.js
+++ ensureQuestion.js
@@ -1,4 +1,5 @@
 // Ensure question
+// https://www.codewars.com/kata/5866fc43395d9138a7000006/elixir
 // Given a string, write a function that returns the string with a question mark ("?") appends to the end, unless the original string ends with a question mark, in which case, returns the original string.
 // For example (Input --> Output)
 

352371be34bc7d00e4dff165a46437c67b492d51
Nathan Truong <88857875+nktruong@users.noreply.github.com>
2021-11-12 23:19:18 -0500

    Update stringTransformer.js

--- stringTransformer.js
+++ stringTransformer.js
@@ -1,3 +1,6 @@
+// String transformer
+// https://www.codewars.com/kata/5878520d52628a092f0002d0
+
 // Given a string, return a new string that has transformed based on the input:
 
 // Change case of every character, ie. lower case to upper case, upper case to lower case.

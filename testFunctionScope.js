7c32dc249bd7aa12818cbe05fb1ff878028ca8ad
Nathan Truong <88857875+nktruong@users.noreply.github.com>
2021-11-12 23:22:31 -0500

    Update testFunctionScope.js

--- testFunctionScope.js
+++ testFunctionScope.js
@@ -1,4 +1,6 @@
 // Test Your Knowledge Of Function Scope
+// https://www.codewars.com/kata/56d344c7fd3a52566700124b/javascript
+
 // Write a function that adds from two invocations.
 
 // All inputs will be integers.

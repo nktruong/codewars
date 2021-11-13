9b989febb605d99c78a7e467b897b6341d3ae617
Nathan Truong <88857875+nktruong@users.noreply.github.com>
2021-11-12 22:50:06 -0500

    Update boolToWord.js

--- boolToWord.js
+++ boolToWord.js
@@ -1,3 +1,8 @@
+// Convert boolean values to strings 'Yes' or 'No'.
+// https://www.codewars.com/kata/53369039d7ab3ac506000467/javascript
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
 function boolToWord( bool ){
   return bool ? "Yes" : "No" 
 }

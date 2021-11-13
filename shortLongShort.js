3b56cbdd9b1f3ea2241b3c17614a5931c191080b
Nathan Truong <88857875+nktruong@users.noreply.github.com>
2021-11-12 23:17:19 -0500

    Update shortLongShort.js

--- shortLongShort.js
+++ shortLongShort.js
@@ -1,4 +1,6 @@
-// ### Short Long Short
+// Short Long Short
+// https://www.codewars.com/kata/50654ddff44f800200000007
+
 // Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside.
 // The strings will not be the same length, but they may be empty ( zero length ).
 

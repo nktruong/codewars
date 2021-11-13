9f1864e20f6c39355b833eab1a7f3e099c741f9e
Nathan Truong <88857875+nktruong@users.noreply.github.com>
2021-11-12 23:21:46 -0500

    Update sumOfPositive.js

--- sumOfPositive.js
+++ sumOfPositive.js
@@ -1,9 +1,12 @@
 // Sum of Positive
+// https://www.codewars.com/kata/5715eaedb436cf5606000381
+
 // You get an array of numbers, return the sum of all of the positives ones.
 
 // Example [1,-4,7,12] => 1 + 7 + 12 = 20
 
 // Note: if there is nothing to sum, the sum is default to 0.
+
 function positiveSum(arr) {
   let sum = 0
   arr.forEach(num => num > 0 ? sum+=num : sum)

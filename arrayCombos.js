12200c9f36a0af03b3fb9ff67d17641cc74eb6a6
Nathan Truong <88857875+nktruong@users.noreply.github.com>
2021-11-12 23:36:39 -0500

    Update arrayCombos.js

--- arrayCombos.js
+++ arrayCombos.js
@@ -1,5 +1,5 @@
 // Array combinations
-// https://www.codewars.com/kata/59e66e48fc3c499ec5000103/solutions/javascript
+// https://www.codewars.com/kata/59e66e48fc3c499ec5000103/javascript
 
 // In this Kata, you will be given an array of arrays and your task will be to return the number of unique arrays that can be formed by picking exactly one element from each subarray.
 // For example: solve([[1,2],[4],[5,6]]) = 4, because it results in only 4 possibilites. They are [1,4,5],[1,4,6],[2,4,5],[2,4,6].

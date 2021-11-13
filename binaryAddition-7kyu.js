4155b086188674f6dd435635d73587c7766037ae
Nathan Truong <88857875+nktruong@users.noreply.github.com>
2021-11-12 22:48:29 -0500

    Update binaryAddition-7kyu.js

--- binaryAddition-7kyu.js
+++ binaryAddition-7kyu.js
@@ -1,4 +1,5 @@
 // ### Binary Addition
+// https://www.codewars.com/kata/551f37452ff852b7bd000139
 // Sept 8 2021
 
 // Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.

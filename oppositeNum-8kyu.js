fa2378bd5a3cf780ccef3153a1bb62ceef1baee0
Nathan Truong <88857875+nktruong@users.noreply.github.com>
2021-11-12 23:13:52 -0500

    Update oppositeNum-8kyu.js

--- oppositeNum-8kyu.js
+++ oppositeNum-8kyu.js
@@ -1,3 +1,14 @@
+// Opposite number
+// https://www.codewars.com/kata/56dec885c54a926dcd001095/javascript
+
+// Very simple, given a number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
 function opposite(number) {
   return -number
 }

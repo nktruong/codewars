6325e1fc2e31b4f8a317d123ac7328b90474d94a
Nathan Truong <88857875+nktruong@users.noreply.github.com>
2022-01-04 04:28:27 -0500

    simple reverse for loop 01/03/2022

--- temporary.js
+++ temporary.js
@@ -1,1 +1,22 @@
+// Grasshopper - Summation
+// https://www.codewars.com/kata/55d24f55d7dd296eb9000030
 
+// Summation
+
+// Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+// For example:
+
+// summation(2) -> 3
+// 1 + 2
+
+// summation(8) -> 36
+// 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
+var summation = function (num) {
+  let sum = 0
+  for(let i=num; i>0; i--){
+    sum += num
+    num--
+  }
+  return sum
+}
